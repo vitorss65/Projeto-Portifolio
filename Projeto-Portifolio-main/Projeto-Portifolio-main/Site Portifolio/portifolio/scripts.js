d77624d14ff5a89509b5dca2bb342c8748eb5989
Vítor de Souza <115593413+vitorss65@users.noreply.github.com>
2025-02-21 15:10:41 -0300

    Delete Projeto-Portifolio-main/Projeto-Portifolio-main directory

--- Projeto-Portifolio-main/Projeto-Portifolio-main/Site Portifolio/portifolio/scripts.js
+++
@@ -1,9 +0,0 @@
-let skills = document.querySelector('.skills');
-let skillWrapper = document.querySelector('.skill-wrapper');
-let skillsClone = Array.from(skillWrapper.children);
-
-skillsClone.forEach((skill) => {
-    let elemDuplicado = skill.cloneNode(true);
-    elemDuplicado.setAttribute('aria-hidden', 'true');
-    skillWrapper.appendChild(elemDuplicado);
-});
